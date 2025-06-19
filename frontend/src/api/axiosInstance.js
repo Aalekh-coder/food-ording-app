130dc6c85899e4e652933f84913024bb6a1212af
Aalekh-coder <dheetaalekh@gmail.com>
2025-06-19 17:50:10 +0530

    frontend error fixed

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL :"http://localhost:3000"
+    baseURL :"https://food-ording-app.onrender.com",
+    // baseURL :"http://localhost:3000",
 });
 
 export default axiosInstance
